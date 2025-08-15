35924249a62ff7d2510c08cce22db734c43b9a69
John Von Seggern <vonseg@protonmail.com>
2025-08-15 09:19:25 -0500

    Keep debugging.

--- firmware/src/usb_host.cc
+++ firmware/src/usb_host.cc
@@ -45,9 +45,7 @@ void tuh_hid_mount_cb(uint8_t dev_addr, uint8_t instance, uint8_t const* desc_re
         event.data.device.device_type = "UNKNOWN";
     }
     
-    usb_event_queue_push(&g_event_queue, &event);
-    
-    // Parse HID descriptor if available
+    // Parse HID descriptor if available first, before pushing event
     if (desc_report && desc_len > 0 && instance < CFG_TUH_HID) {
         if (hid_parse_descriptor(desc_report, desc_len, &g_hid_descriptors[instance])) {
             // Successfully parsed descriptor
@@ -59,6 +57,8 @@ void tuh_hid_mount_cb(uint8_t dev_addr, uint8_t instance, uint8_t const* desc_re
     } else {
         event.data.device.device_type = protocol_str[itf_protocol];
     }
+    
+    usb_event_queue_push(&g_event_queue, &event);
     
     tuh_hid_receive_report(dev_addr, instance);
 }
@@ -121,79 +121,13 @@ void tuh_hid_report_received_cb(uint8_t dev_addr, uint8_t instance, uint8_t cons
         }
         
         default:
-            // Use HID descriptor parser if available
-            if (instance < CFG_TUH_HID && g_hid_descriptors[instance].usage_count > 0) {
-                hid_descriptor_t* desc = &g_hid_descriptors[instance];
-                
-                // Process each usage in the descriptor
-                for (int i = 0; i < desc->usage_count; i++) {
-                    hid_usage_t* usage = &desc->usages[i];
-                    int32_t value;
-                    
-                    if (hid_extract_value(report, len, usage, &value)) {
-                        input_type_t input_type = hid_get_input_type(usage);
-                        
-                        // Only create events for non-zero values (activity)
-                        if (value != 0) {
-                            switch (input_type) {
-                                case INPUT_TYPE_MOUSE_X:
-                                    event.type = USB_EVENT_MOUSE;
-                                    event.data.mouse.delta_x = value;
-                                    event.data.mouse.delta_y = 0;
-                                    event.data.mouse.buttons = 0;
-                                    event.data.mouse.scroll = 0;
-                                    usb_event_queue_push(&g_event_queue, &event);
-                                    break;
-                                    
-                                case INPUT_TYPE_MOUSE_Y:
-                                    event.type = USB_EVENT_MOUSE;
-                                    event.data.mouse.delta_x = 0;
-                                    event.data.mouse.delta_y = value;
-                                    event.data.mouse.buttons = 0;
-                                    event.data.mouse.scroll = 0;
-                                    usb_event_queue_push(&g_event_queue, &event);
-                                    break;
-                                    
-                                case INPUT_TYPE_MOUSE_BUTTON:
-                                    event.type = USB_EVENT_MOUSE;
-                                    event.data.mouse.delta_x = 0;
-                                    event.data.mouse.delta_y = 0;
-                                    event.data.mouse.buttons = value;
-                                    event.data.mouse.scroll = 0;
-                                    usb_event_queue_push(&g_event_queue, &event);
-                                    break;
-                                    
-                                case INPUT_TYPE_KEYBOARD_KEY:
-                                    event.type = USB_EVENT_KEYBOARD;
-                                    event.data.keyboard.keycode = value;
-                                    event.data.keyboard.modifier = 0;
-                                    event.data.keyboard.pressed = true;
-                                    usb_event_queue_push(&g_event_queue, &event);
-                                    break;
-                                    
-                                case INPUT_TYPE_KEYBOARD_MODIFIER:
-                                    event.type = USB_EVENT_KEYBOARD;
-                                    event.data.keyboard.keycode = 0;
-                                    event.data.keyboard.modifier = value;
-                                    event.data.keyboard.pressed = true;
-                                    usb_event_queue_push(&g_event_queue, &event);
-                                    break;
-                                    
-                                default:
-                                    break;
-                            }
-                        }
-                    }
-                }
-            } else {
-                // Fallback to raw data display if no descriptor
-                if (len > 0) {
-                    event.type = USB_EVENT_KEYBOARD;
-                    event.data.keyboard.keycode = report[0];
-                    event.data.keyboard.modifier = (len > 1) ? report[1] : 0;
-                    event.data.keyboard.pressed = true;
-                    usb_event_queue_push(&g_event_queue, &event);
-                }
+            // Temporary: Go back to simple raw data display to debug
+            if (len > 0) {
+                event.type = USB_EVENT_KEYBOARD;
+                event.data.keyboard.keycode = report[0];
+                event.data.keyboard.modifier = (len > 1) ? report[1] : 0;
+                event.data.keyboard.pressed = true;
+                usb_event_queue_push(&g_event_queue, &event);
             }
             break;
     }
