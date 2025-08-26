2558bdc9006f28e0a33f7ae8626e6a565100a80e
John Von Seggern <vonseg@protonmail.com>
2025-08-25 23:40:32 -0500

    Fiddle with formatting.

--- firmware/src/display/display.c
+++ firmware/src/display/display.c
@@ -12,11 +12,13 @@ bool display_init(i2c_inst_t *i2c) {
     return display_ok;
 }
 
+
 void display_clear_buffer(void) {
     if (!display_ok) return;
 
     sh1107_clear(&display);
 }
+
 
 void display_show_buffer(void) {
     if (!display_ok) return;
@@ -24,17 +26,20 @@ void display_show_buffer(void) {
     sh1107_display(&display);
 }
 
+
 void display_copy_pixels(const uint8_t *pixel_data) {
     if (!display_ok) return;
 
     sh1107_draw_buffer(&display, pixel_data);
 }
 
+
 void display_draw_char(int16_t x, int16_t y, char c) {
     if (!display_ok) return;
 
     sh1107_draw_char(&display, x, y, c);
 }
+
 
 void display_draw_string(int16_t x, int16_t y, const char *str) {
     if (!display_ok) return;
